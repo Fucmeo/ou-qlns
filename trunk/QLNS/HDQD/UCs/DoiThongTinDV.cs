5218d558ed6593fbd81913272f8f8ddeb4ab77ad
khangtran1990@gmail.com <khangtran1990@gmail.com@2fc767be-672c-329e-4b41-85d192040dfc>
2013-02-07 13:47:05 +0000

    Update giao diện DoiThongTinDV

--- trunk/QLNS/HDQD/UCs/DoiThongTinDV.cs
+++ trunk/QLNS/HDQD/UCs/DoiThongTinDV.cs
@@ -6,6 +6,7 @@ using System.Data;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using Business;
 
 namespace HDQD.UCs
 {
@@ -14,6 +15,11 @@ namespace HDQD.UCs
         public DoiThongTinDV()
         {
             InitializeComponent();
+        }
+
+        private void DoiThongTinDV_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
