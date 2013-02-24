d2cee832ac618f27c11f2e0e5ed8ad72ccdaf5e1
khangtran1990@gmail.com <khangtran1990@gmail.com@2fc767be-672c-329e-4b41-85d192040dfc>
2013-02-24 17:47:24 +0000

    Them DSTapTin

--- trunk/QLNS/HDQD/Forms/Popup.designer.cs
+++ trunk/QLNS/HDQD/Forms/Popup.designer.cs
@@ -32,13 +32,12 @@
             // 
             // Popup
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(644, 622);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.ClientSize = new System.Drawing.Size(859, 766);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Popup";
             this.Text = "Popup";
-            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.Load += new System.EventHandler(this.Popup_Load);
             this.ResumeLayout(false);
 
