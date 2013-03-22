055819de7ff12a73446b3ef5842d25722341bbbe
khangtran1990 <khangtran1990@gmail.com>
2013-03-22 09:01:11 +0000

    Add QLNS_ChucVu_ChinhTri

---
+++ QLNS/QLNS/UCs/QLNS_ChucVu_ChinhTri.Designer.cs
@@ -0,0 +1,340 @@
+﻿namespace QLNS.UCs
+{
+    partial class QLNS_ChucVu_ChinhTri
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(QLNS_ChucVu_ChinhTri));
+            this.tableLP_ChucVu_ChinhTri = new System.Windows.Forms.TableLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.tableLP_ThaoTac = new System.Windows.Forms.TableLayoutPanel();
+            this.btn_Sua = new System.Windows.Forms.Button();
+            this.btn_Xoa = new System.Windows.Forms.Button();
+            this.btn_Them = new System.Windows.Forms.Button();
+            this.tableLP_LuuHuy = new System.Windows.Forms.TableLayoutPanel();
+            this.btn_Huy = new System.Windows.Forms.Button();
+            this.btn_Luu = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.tableLP_DSVanBang = new System.Windows.Forms.TableLayoutPanel();
+            this.dtgv_DSChucVuDonVi = new System.Windows.Forms.DataGridView();
+            this.txt_Ten = new System.Windows.Forms.TextBox();
+            this.comB_Loai = new System.Windows.Forms.ComboBox();
+            this.tableLP_ChucVu_ChinhTri.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.tableLP_ThaoTac.SuspendLayout();
+            this.tableLP_LuuHuy.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.tableLP_DSVanBang.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dtgv_DSChucVuDonVi)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tableLP_ChucVu_ChinhTri
+            // 
+            this.tableLP_ChucVu_ChinhTri.ColumnCount = 4;
+            this.tableLP_ChucVu_ChinhTri.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 15F));
+            this.tableLP_ChucVu_ChinhTri.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 35F));
+            this.tableLP_ChucVu_ChinhTri.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 15F));
+            this.tableLP_ChucVu_ChinhTri.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 35F));
+            this.tableLP_ChucVu_ChinhTri.Controls.Add(this.comB_Loai, 3, 0);
+            this.tableLP_ChucVu_ChinhTri.Controls.Add(this.txt_Ten, 1, 0);
+            this.tableLP_ChucVu_ChinhTri.Controls.Add(this.groupBox1, 0, 2);
+            this.tableLP_ChucVu_ChinhTri.Controls.Add(this.groupBox2, 0, 1);
+            this.tableLP_ChucVu_ChinhTri.Controls.Add(this.label1, 0, 0);
+            this.tableLP_ChucVu_ChinhTri.Controls.Add(this.label2, 2, 0);
+            this.tableLP_ChucVu_ChinhTri.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLP_ChucVu_ChinhTri.GrowStyle = System.Windows.Forms.TableLayoutPanelGrowStyle.FixedSize;
+            this.tableLP_ChucVu_ChinhTri.Location = new System.Drawing.Point(0, 0);
+            this.tableLP_ChucVu_ChinhTri.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.tableLP_ChucVu_ChinhTri.Name = "tableLP_ChucVu_ChinhTri";
+            this.tableLP_ChucVu_ChinhTri.RowCount = 3;
+            this.tableLP_ChucVu_ChinhTri.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLP_ChucVu_ChinhTri.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 40F));
+            this.tableLP_ChucVu_ChinhTri.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 40F));
+            this.tableLP_ChucVu_ChinhTri.Size = new System.Drawing.Size(600, 600);
+            this.tableLP_ChucVu_ChinhTri.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 18);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Tên chức vụ";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(303, 51);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(33, 18);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Loại";
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "Add.png");
+            this.imageList1.Images.SetKeyName(1, "Cancel.png");
+            this.imageList1.Images.SetKeyName(2, "Edit Data.png");
+            this.imageList1.Images.SetKeyName(3, "Garbage.png");
+            this.imageList1.Images.SetKeyName(4, "Save.png");
+            // 
+            // groupBox2
+            // 
+            this.tableLP_ChucVu_ChinhTri.SetColumnSpan(this.groupBox2, 4);
+            this.groupBox2.Controls.Add(this.tableLP_ThaoTac);
+            this.groupBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox2.Location = new System.Drawing.Point(3, 124);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.groupBox2.Size = new System.Drawing.Size(594, 232);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Thao tác";
+            // 
+            // tableLP_ThaoTac
+            // 
+            this.tableLP_ThaoTac.ColumnCount = 3;
+            this.tableLP_ThaoTac.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLP_ThaoTac.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLP_ThaoTac.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLP_ThaoTac.Controls.Add(this.btn_Sua, 2, 0);
+            this.tableLP_ThaoTac.Controls.Add(this.btn_Xoa, 1, 0);
+            this.tableLP_ThaoTac.Controls.Add(this.btn_Them, 0, 0);
+            this.tableLP_ThaoTac.Controls.Add(this.tableLP_LuuHuy, 1, 1);
+            this.tableLP_ThaoTac.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLP_ThaoTac.Location = new System.Drawing.Point(3, 23);
+            this.tableLP_ThaoTac.Name = "tableLP_ThaoTac";
+            this.tableLP_ThaoTac.RowCount = 2;
+            this.tableLP_ThaoTac.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLP_ThaoTac.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLP_ThaoTac.Size = new System.Drawing.Size(588, 205);
+            this.tableLP_ThaoTac.TabIndex = 0;
+            // 
+            // btn_Sua
+            // 
+            this.btn_Sua.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btn_Sua.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btn_Sua.ImageAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btn_Sua.ImageKey = "Edit Data.png";
+            this.btn_Sua.ImageList = this.imageList1;
+            this.btn_Sua.Location = new System.Drawing.Point(440, 3);
+            this.btn_Sua.Name = "btn_Sua";
+            this.btn_Sua.Size = new System.Drawing.Size(100, 96);
+            this.btn_Sua.TabIndex = 3;
+            this.btn_Sua.Text = "Sửa";
+            this.btn_Sua.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btn_Sua.UseVisualStyleBackColor = true;
+            // 
+            // btn_Xoa
+            // 
+            this.btn_Xoa.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btn_Xoa.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btn_Xoa.ImageAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btn_Xoa.ImageKey = "Garbage.png";
+            this.btn_Xoa.ImageList = this.imageList1;
+            this.btn_Xoa.Location = new System.Drawing.Point(244, 3);
+            this.btn_Xoa.Name = "btn_Xoa";
+            this.btn_Xoa.Size = new System.Drawing.Size(100, 96);
+            this.btn_Xoa.TabIndex = 2;
+            this.btn_Xoa.Text = "Xoá";
+            this.btn_Xoa.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btn_Xoa.UseVisualStyleBackColor = true;
+            // 
+            // btn_Them
+            // 
+            this.btn_Them.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btn_Them.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btn_Them.ImageAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btn_Them.ImageKey = "Add.png";
+            this.btn_Them.ImageList = this.imageList1;
+            this.btn_Them.Location = new System.Drawing.Point(48, 3);
+            this.btn_Them.Name = "btn_Them";
+            this.btn_Them.Size = new System.Drawing.Size(100, 96);
+            this.btn_Them.TabIndex = 0;
+            this.btn_Them.Text = "Thêm";
+            this.btn_Them.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btn_Them.UseVisualStyleBackColor = true;
+            // 
+            // tableLP_LuuHuy
+            // 
+            this.tableLP_LuuHuy.ColumnCount = 2;
+            this.tableLP_LuuHuy.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLP_LuuHuy.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLP_LuuHuy.Controls.Add(this.btn_Huy, 1, 0);
+            this.tableLP_LuuHuy.Controls.Add(this.btn_Luu, 0, 0);
+            this.tableLP_LuuHuy.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLP_LuuHuy.Location = new System.Drawing.Point(199, 105);
+            this.tableLP_LuuHuy.Name = "tableLP_LuuHuy";
+            this.tableLP_LuuHuy.RowCount = 1;
+            this.tableLP_LuuHuy.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLP_LuuHuy.Size = new System.Drawing.Size(190, 97);
+            this.tableLP_LuuHuy.TabIndex = 1;
+            // 
+            // btn_Huy
+            // 
+            this.btn_Huy.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btn_Huy.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btn_Huy.ImageAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btn_Huy.ImageKey = "Cancel.png";
+            this.btn_Huy.ImageList = this.imageList1;
+            this.btn_Huy.Location = new System.Drawing.Point(98, 3);
+            this.btn_Huy.Name = "btn_Huy";
+            this.btn_Huy.Size = new System.Drawing.Size(89, 91);
+            this.btn_Huy.TabIndex = 5;
+            this.btn_Huy.Text = "Huỷ";
+            this.btn_Huy.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btn_Huy.UseVisualStyleBackColor = true;
+            // 
+            // btn_Luu
+            // 
+            this.btn_Luu.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btn_Luu.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btn_Luu.ImageAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btn_Luu.ImageKey = "Save.png";
+            this.btn_Luu.ImageList = this.imageList1;
+            this.btn_Luu.Location = new System.Drawing.Point(3, 3);
+            this.btn_Luu.Name = "btn_Luu";
+            this.btn_Luu.Size = new System.Drawing.Size(89, 91);
+            this.btn_Luu.TabIndex = 4;
+            this.btn_Luu.Text = "Lưu";
+            this.btn_Luu.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btn_Luu.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.tableLP_ChucVu_ChinhTri.SetColumnSpan(this.groupBox1, 4);
+            this.groupBox1.Controls.Add(this.tableLP_DSVanBang);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox1.Location = new System.Drawing.Point(3, 363);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(594, 234);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Danh sách chức vụ - đơn vị";
+            // 
+            // tableLP_DSVanBang
+            // 
+            this.tableLP_DSVanBang.ColumnCount = 2;
+            this.tableLP_DSVanBang.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 18.31395F));
+            this.tableLP_DSVanBang.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 81.68604F));
+            this.tableLP_DSVanBang.Controls.Add(this.dtgv_DSChucVuDonVi, 0, 0);
+            this.tableLP_DSVanBang.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLP_DSVanBang.Location = new System.Drawing.Point(3, 22);
+            this.tableLP_DSVanBang.Name = "tableLP_DSVanBang";
+            this.tableLP_DSVanBang.RowCount = 1;
+            this.tableLP_DSVanBang.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 90F));
+            this.tableLP_DSVanBang.Size = new System.Drawing.Size(588, 209);
+            this.tableLP_DSVanBang.TabIndex = 0;
+            // 
+            // dtgv_DSChucVuDonVi
+            // 
+            this.dtgv_DSChucVuDonVi.AllowUserToAddRows = false;
+            this.dtgv_DSChucVuDonVi.AllowUserToDeleteRows = false;
+            this.dtgv_DSChucVuDonVi.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.tableLP_DSVanBang.SetColumnSpan(this.dtgv_DSChucVuDonVi, 2);
+            this.dtgv_DSChucVuDonVi.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dtgv_DSChucVuDonVi.Location = new System.Drawing.Point(3, 3);
+            this.dtgv_DSChucVuDonVi.MultiSelect = false;
+            this.dtgv_DSChucVuDonVi.Name = "dtgv_DSChucVuDonVi";
+            this.dtgv_DSChucVuDonVi.ReadOnly = true;
+            this.dtgv_DSChucVuDonVi.RowTemplate.Height = 24;
+            this.dtgv_DSChucVuDonVi.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dtgv_DSChucVuDonVi.Size = new System.Drawing.Size(582, 203);
+            this.dtgv_DSChucVuDonVi.TabIndex = 1;
+            // 
+            // txt_Ten
+            // 
+            this.txt_Ten.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.txt_Ten.Location = new System.Drawing.Point(93, 47);
+            this.txt_Ten.MaxLength = 30;
+            this.txt_Ten.Name = "txt_Ten";
+            this.txt_Ten.Size = new System.Drawing.Size(204, 26);
+            this.txt_Ten.TabIndex = 5;
+            // 
+            // comB_Loai
+            // 
+            this.comB_Loai.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.comB_Loai.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comB_Loai.FormattingEnabled = true;
+            this.comB_Loai.Location = new System.Drawing.Point(393, 47);
+            this.comB_Loai.Name = "comB_Loai";
+            this.comB_Loai.Size = new System.Drawing.Size(204, 26);
+            this.comB_Loai.TabIndex = 8;
+            // 
+            // QLNS_ChucVu_ChinhTri
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.Controls.Add(this.tableLP_ChucVu_ChinhTri);
+            this.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "QLNS_ChucVu_ChinhTri";
+            this.Size = new System.Drawing.Size(600, 600);
+            this.tableLP_ChucVu_ChinhTri.ResumeLayout(false);
+            this.tableLP_ChucVu_ChinhTri.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.tableLP_ThaoTac.ResumeLayout(false);
+            this.tableLP_LuuHuy.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.tableLP_DSVanBang.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dtgv_DSChucVuDonVi)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLP_ChucVu_ChinhTri;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TableLayoutPanel tableLP_ThaoTac;
+        private System.Windows.Forms.Button btn_Sua;
+        private System.Windows.Forms.Button btn_Xoa;
+        private System.Windows.Forms.Button btn_Them;
+        private System.Windows.Forms.TableLayoutPanel tableLP_LuuHuy;
+        private System.Windows.Forms.Button btn_Huy;
+        private System.Windows.Forms.Button btn_Luu;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TableLayoutPanel tableLP_DSVanBang;
+        private System.Windows.Forms.DataGridView dtgv_DSChucVuDonVi;
+        private System.Windows.Forms.TextBox txt_Ten;
+        private System.Windows.Forms.ComboBox comB_Loai;
+    }
+}
