f9a4c7b9d0d7791fc9df14a04ba8b5addaa7acbd
khangtran1990 <khangtran1990@gmail.com>
2013-05-21 14:34:01 +0000

    Ra soát lại vụ đơn vị

--- QLNS/Business/FTP.cs
+++ QLNS/Business/FTP.cs
@@ -6,16 +6,20 @@ using System.IO;
 using System.Net;
 using System.Runtime.InteropServices;
 
+
 namespace Business
 {
     public class FTP
     {
         public enum FileCate { HinhDaiDien, HDQD };
-        string URI = "ftp://10.1.12.6/", globalFolderName, downloadPath  ;
+        string URI = 
+            //"ftp://10.1.12.6/",
+            "ftp://123.30.210.98/",
+            globalFolderName, downloadPath  ;
         string UserName = "Administrator",
-            Password = "QLNS@123qlns"
-            //Password = "A@123456"
-            ;
+            Password = "QLNS@123qlns";
+            //Password = "A@123456";
+
         public FileCate oFileCate = new FileCate();
         long lFileSizeLimit = 2621440;  // 2,5 mb
         public FTP()
