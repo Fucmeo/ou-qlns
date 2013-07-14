ee62396413127e946ca5ed82029bc088a1e9db8d
fucmeo <fucmeo@gmail.com>
2013-07-14 18:12:52 +0000

    Update: - Business/HDQD/QuyetDinh.cs - HDQD/UCs/QDChung.cs - HDQD/Forms/Popup.cs

--- QLNS/HDQD/UCs/QDChung.cs
+++ QLNS/HDQD/UCs/QDChung.cs
@@ -24,6 +24,9 @@ namespace HDQD.UCs
         DataTable dtPhuCap;
         int row_count_pc = 0;
 
+        string chuoi_cong_thuc = "";
+        string chuoi_cong_thuc_text = "";
+
         public QDChung()
         {
             InitializeComponent();
@@ -100,41 +103,44 @@ namespace HDQD.UCs
 
         private void EditInterface_LoaiPC()
         {
-            int id = Convert.ToInt16(comB_LoaiPhuCap.SelectedValue.ToString());
-            int cach_tinh = Convert.ToInt16((from c in dtLoaiPC.AsEnumerable()
-                                             where c.Field<int>("id") == id
-                                             select c.Field<int>("cach_tinh")).ElementAt(0).ToString());
-
-            string cong_thuc = (from c in dtLoaiPC.AsEnumerable()
-                                where c.Field<int>("id") == id
-                                select c.Field<string>("chuoi_cong_thuc")).ElementAt(0).ToString();
-            switch (cach_tinh)
+            if (cb_CoPhuCap.Checked == false)
             {
-                case 1:
-                    txt_TienPC.Enabled = true;
-                    txt_HeSoPC.Enabled = false;
-                    nup_Value_PhanTramPC.Enabled = false;
-                    break;
-                case 2:
-                    txt_TienPC.Enabled = false;
-                    txt_HeSoPC.Enabled = true;
-                    txt_Luong_PC.Text = "Lương cơ bản";
-                    nup_Value_PhanTramPC.Enabled = false;
-                    break;
-                case 3:
-                    txt_TienPC.Enabled = false;
-                    txt_HeSoPC.Enabled = true;
-                    txt_Luong_PC.Text = "Lương tối thiểu";
-                    nup_Value_PhanTramPC.Enabled = false;
-                    break;
-                case 4:
-                    txt_TienPC.Enabled = false;
-                    txt_HeSoPC.Enabled = false;
-                    nup_Value_PhanTramPC.Enabled = true;
-                    txt_CongThucPC.Text = cong_thuc;
-                    break;
-                default:
-                    break;
+                int id = Convert.ToInt16(comB_LoaiPhuCap.SelectedValue.ToString());
+                int cach_tinh = Convert.ToInt16((from c in dtLoaiPC.AsEnumerable()
+                                                 where c.Field<int>("id") == id
+                                                 select c.Field<int>("cach_tinh")).ElementAt(0).ToString());
+
+                string cong_thuc = (from c in dtLoaiPC.AsEnumerable()
+                                    where c.Field<int>("id") == id
+                                    select c.Field<string>("chuoi_cong_thuc")).ElementAt(0).ToString();
+                switch (cach_tinh)
+                {
+                    case 1:
+                        txt_TienPC.Enabled = true;
+                        txt_HeSoPC.Enabled = false;
+                        nup_Value_PhanTramPC.Enabled = false;
+                        break;
+                    case 2:
+                        txt_TienPC.Enabled = false;
+                        txt_HeSoPC.Enabled = true;
+                        txt_Luong_PC.Text = "Lương cơ bản";
+                        nup_Value_PhanTramPC.Enabled = false;
+                        break;
+                    case 3:
+                        txt_TienPC.Enabled = false;
+                        txt_HeSoPC.Enabled = true;
+                        txt_Luong_PC.Text = "Lương tối thiểu";
+                        nup_Value_PhanTramPC.Enabled = false;
+                        break;
+                    case 4:
+                        txt_TienPC.Enabled = false;
+                        txt_HeSoPC.Enabled = false;
+                        nup_Value_PhanTramPC.Enabled = true;
+                        txt_CongThucPC.Text = cong_thuc;
+                        break;
+                    default:
+                        break;
+                }
             }
         }
 
@@ -209,7 +215,7 @@ namespace HDQD.UCs
 
         private bool CheckInputData()
         {
-            if (thongTinCNVC1.txt_MaNV.Text != "" && dtPhuCap.Rows.Count > 0)
+            if (thongTinCNVC1.txt_MaNV.Text != "" && thongTinQuyetDinh1.txt_MaQD.Text != "" && dtPhuCap.Rows.Count > 0)
                 return true;
             else
                 return false;
@@ -322,37 +328,52 @@ namespace HDQD.UCs
                     #endregion
 
                     dr["co_phu_cap"] = !cb_CoPhuCap.Checked;
-                    dr["loai_pc_id"] = comB_LoaiPhuCap.SelectedValue;
-                    dr["ten_loai"] = comB_LoaiPhuCap.Text;
-                    if (String.IsNullOrEmpty(txt_TienPC.Text))
-                        dr["value_khoan"] = DBNull.Value;
-                    else
-                        dr["value_khoan"] = Convert.ToDouble(txt_TienPC.Text);
-
-                    if (String.IsNullOrEmpty(txt_HeSoPC.Text))
-                        dr["value_he_so"] = DBNull.Value;
-                    else
-                        dr["value_he_so"] = Convert.ToDouble(txt_HeSoPC.Text);
-
-                    if (String.IsNullOrEmpty(nup_Value_PhanTramPC.Text))
-                        dr["value_phan_tram"] = DBNull.Value;
-                    else
-                        dr["value_phan_tram"] = Convert.ToDouble(nup_Value_PhanTramPC.Text);
-
-                    dr["phan_tram_huong"] = Convert.ToDouble(nup_PhanTramPC.Value);
-
-                    try
+                    if (!cb_CoPhuCap.Checked)
                     {
-                        dr["tu_ngay"] = dTP_NgayBatDauPC.Value;
-
-                        if (dTP_NgayHetHanPC.Checked)
-                            dr["den_ngay"] = dTP_NgayHetHanPC.Value;
+                        dr["loai_pc_id"] = comB_LoaiPhuCap.SelectedValue;
+                        dr["ten_loai"] = comB_LoaiPhuCap.Text;
+                        if (String.IsNullOrEmpty(txt_TienPC.Text))
+                            dr["value_khoan"] = DBNull.Value;
                         else
-                            dr["den_ngay"] = DBNull.Value;
-                    }
-                    catch { }
+                            dr["value_khoan"] = Convert.ToDouble(txt_TienPC.Text);
 
-                    dr["ghi_chu"] = rTB_GhiChuPC.Text;
+                        if (String.IsNullOrEmpty(txt_HeSoPC.Text))
+                            dr["value_he_so"] = DBNull.Value;
+                        else
+                            dr["value_he_so"] = Convert.ToDouble(txt_HeSoPC.Text);
+
+                        if (String.IsNullOrEmpty(nup_Value_PhanTramPC.Text))
+                            dr["value_phan_tram"] = DBNull.Value;
+                        else
+                            dr["value_phan_tram"] = Convert.ToDouble(nup_Value_PhanTramPC.Text);
+
+                        dr["phan_tram_huong"] = Convert.ToDouble(nup_PhanTramPC.Value);
+
+                        try
+                        {
+                            dr["tu_ngay"] = dTP_NgayBatDauPC.Value;
+
+                            if (dTP_NgayHetHanPC.Checked)
+                                dr["den_ngay"] = dTP_NgayHetHanPC.Value;
+                            else
+                                dr["den_ngay"] = DBNull.Value;
+                        }
+                        catch { }
+
+                        dr["ghi_chu"] = rTB_GhiChuPC.Text;
+                    }
+                    else
+                    {
+                        dr["loai_pc_id"] = DBNull.Value;
+                        dr["ten_loai"] = DBNull.Value;
+                        dr["value_khoan"] = DBNull.Value;
+                        dr["value_he_so"] = DBNull.Value;
+                        dr["value_phan_tram"] = DBNull.Value;
+                        dr["phan_tram_huong"] = DBNull.Value;
+                        dr["tu_ngay"] = DBNull.Value;
+                        dr["den_ngay"] = DBNull.Value;
+                        dr["ghi_chu"] = DBNull.Value;
+                    }
 
                     dtPhuCap.Rows.Add(dr);
                     row_count_pc++;
@@ -383,8 +404,8 @@ namespace HDQD.UCs
 
         private void cb_CoPhuCap_CheckedChanged(object sender, EventArgs e)
         {
-            comB_LoaiPhuCap.Enabled = txt_TienPC.Enabled = txt_HeSoPC.Enabled = dTP_NgayBatDauPC.Enabled =
-                dTP_NgayHetHanPC.Enabled = nup_PhanTramPC.Enabled = rTB_GhiChuPC.Enabled = dtgv_DSPhuCap.Enabled = !cb_CoPhuCap.Checked;
+            comB_LoaiPhuCap.Enabled = txt_TienPC.Enabled = txt_HeSoPC.Enabled = dTP_NgayBatDauPC.Enabled = nup_Value_PhanTramPC.Enabled =
+                dTP_NgayHetHanPC.Enabled = nup_PhanTramPC.Enabled = rTB_GhiChuPC.Enabled = !cb_CoPhuCap.Checked;
             EditInterface_LoaiPC();
         }
 
@@ -397,7 +418,7 @@ namespace HDQD.UCs
                 bool Tham_Nien_Nang_Bac = false;
                 bool Tham_Nien_Nha_Giao = false;
                 bool Define_Cong_Thuc = false;
-                string Cong_Thuc = "";
+                //string Cong_Thuc = "";
                 double? Phan_Tram_Cong_Thuc;
                 bool Khong_Tinh_Luong = false;
 
@@ -447,13 +468,14 @@ namespace HDQD.UCs
                 if (rb_TinhLuong.Checked && rb_CT_Moi.Checked)
                 {
                     Define_Cong_Thuc = true;
-                    Cong_Thuc = txt_CongThucLuong.Text;
+                    //Cong_Thuc = txt_CongThucLuong.Text;
                     Phan_Tram_Cong_Thuc = Convert.ToDouble(nup_Value_PhanTramLuong.Value);
                 }
                 else
                 {
                     Define_Cong_Thuc = false;
-                    Cong_Thuc = null;
+                    chuoi_cong_thuc = null;
+                    chuoi_cong_thuc_text = null;
                     Phan_Tram_Cong_Thuc = null;
                 }
 
@@ -486,66 +508,81 @@ namespace HDQD.UCs
                     #endregion
 
                     #region Phu Cap Info
-                    co_phu_cap.Add(Convert.ToBoolean(dr["co_phu_cap"].ToString()));
-                    int loaipc_id = Convert.ToInt16(dr["loai_pc_id"].ToString());
-                    loai_pc_id.Add(loaipc_id);
-
-                    int cach_tinh = Convert.ToInt16((from c in dtLoaiPC.AsEnumerable()
-                                                     where c.Field<int>("id") == loaipc_id
-                                                     select c.Field<int>("cach_tinh")).ElementAt(0).ToString());
-
-                    switch (cach_tinh)
+                    bool m_co_pc = Convert.ToBoolean(dr["co_phu_cap"].ToString());
+                    co_phu_cap.Add(m_co_pc);
+                    if (m_co_pc)
                     {
-                        case 1:
-                            if (dr["value_khoan"].ToString() == "")
-                                value_khoan.Add(-1);
-                            else
-                                value_khoan.Add(Convert.ToDouble(dr["value_khoan"].ToString()));
+                        int loaipc_id = Convert.ToInt16(dr["loai_pc_id"].ToString());
+                        loai_pc_id.Add(loaipc_id);
 
-                            value_heso.Add(-1);
-                            value_phantram.Add(-1);
-                            break;
-                        case 2:
-                            value_khoan.Add(-1);
+                        int cach_tinh = Convert.ToInt16((from c in dtLoaiPC.AsEnumerable()
+                                                         where c.Field<int>("id") == loaipc_id
+                                                         select c.Field<int>("cach_tinh")).ElementAt(0).ToString());
 
-                            if (dr["value_he_so"].ToString() == "")
+                        switch (cach_tinh)
+                        {
+                            case 1:
+                                if (dr["value_khoan"].ToString() == "")
+                                    value_khoan.Add(-1);
+                                else
+                                    value_khoan.Add(Convert.ToDouble(dr["value_khoan"].ToString()));
+
                                 value_heso.Add(-1);
-                            else
-                                value_heso.Add(Convert.ToDouble(dr["value_he_so"].ToString()));
-
-                            value_phantram.Add(-1);
-                            break;
-                        case 3:
-                            value_khoan.Add(-1);
-
-                            if (dr["value_he_so"].ToString() == "")
-                                value_heso.Add(-1);
-                            else
-                                value_heso.Add(Convert.ToDouble(dr["value_he_so"].ToString()));
-
-                            value_phantram.Add(-1);
-                            break;
-                        case 4:
-                            value_khoan.Add(-1);
-                            value_heso.Add(-1);
-
-                            if (dr["value_phan_tram"].ToString() == "")
                                 value_phantram.Add(-1);
-                            else
-                                value_phantram.Add(Convert.ToDouble(dr["value_phan_tram"].ToString()));
-                            break;
-                        default:
-                            break;
+                                break;
+                            case 2:
+                                value_khoan.Add(-1);
+
+                                if (dr["value_he_so"].ToString() == "")
+                                    value_heso.Add(-1);
+                                else
+                                    value_heso.Add(Convert.ToDouble(dr["value_he_so"].ToString()));
+
+                                value_phantram.Add(-1);
+                                break;
+                            case 3:
+                                value_khoan.Add(-1);
+
+                                if (dr["value_he_so"].ToString() == "")
+                                    value_heso.Add(-1);
+                                else
+                                    value_heso.Add(Convert.ToDouble(dr["value_he_so"].ToString()));
+
+                                value_phantram.Add(-1);
+                                break;
+                            case 4:
+                                value_khoan.Add(-1);
+                                value_heso.Add(-1);
+
+                                if (dr["value_phan_tram"].ToString() == "")
+                                    value_phantram.Add(-1);
+                                else
+                                    value_phantram.Add(Convert.ToDouble(dr["value_phan_tram"].ToString()));
+                                break;
+                            default:
+                                break;
+                        }
+                        phan_tram.Add(Convert.ToDouble(dr["phan_tram_huong"].ToString()));
+                        tu_ngay.Add(Convert.ToDateTime(dr["tu_ngay"].ToString()).Date);
+
+                        if (dr["den_ngay"].ToString() == "")
+                            den_ngay.Add(Convert.ToDateTime("01/01/1901").Date);
+                        else
+                            den_ngay.Add(Convert.ToDateTime(dr["den_ngay"].ToString()).Date);
+
+                        ghi_chu.Add(dr["ghi_chu"].ToString());
                     }
-                    phan_tram.Add(Convert.ToDouble(dr["phan_tram_huong"].ToString()));
-                    tu_ngay.Add(Convert.ToDateTime(dr["tu_ngay"].ToString()).Date);
-
-                    if (dr["den_ngay"].ToString() == "")
-                        den_ngay.Add(Convert.ToDateTime("01/01/1901").Date);
                     else
-                        den_ngay.Add(Convert.ToDateTime(dr["den_ngay"].ToString()).Date);
-
-                    ghi_chu.Add(dr["ghi_chu"].ToString());
+                    {
+                        loai_pc_id.Add(-1);
+                        value_khoan.Add(-1);
+                        value_heso.Add(-1);
+                        value_phantram.Add(-1);
+                        phan_tram.Add(-1);
+                        tu_ngay.Add(Convert.ToDateTime("01/01/1901").Date);
+                        den_ngay.Add(Convert.ToDateTime("01/01/1901").Date);
+                        ghi_chu.Add("");
+                    }
                     #endregion
                 }
 
@@ -553,7 +590,8 @@ namespace HDQD.UCs
                 {
                     if (MessageBox.Show("Bạn thực sự muốn thêm quyết định cho nhân viên này?", "Hỏi", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
                     {
-                        if (oQuyetDinh.Add_QuyetDinhChung(Tham_Nien_Nang_Bac, Tham_Nien_Nha_Giao, Define_Cong_Thuc, Cong_Thuc, Phan_Tram_Cong_Thuc, Khong_Tinh_Luong,
+                        if (oQuyetDinh.Add_QuyetDinhChung(Tham_Nien_Nang_Bac, Tham_Nien_Nha_Giao, Define_Cong_Thuc, chuoi_cong_thuc_text, chuoi_cong_thuc, 
+                                                            Phan_Tram_Cong_Thuc, Khong_Tinh_Luong,
                                                             ma_nv.ToArray(), ins_qtr_ctac.ToArray(), don_vi_id.ToArray(), chuc_vu_id.ToArray(), chuc_danh_id.ToArray(),
                                                             co_phu_cap.ToArray(), loai_pc_id.ToArray(), value_khoan.ToArray(), value_heso.ToArray(),
                                                             value_phantram.ToArray(), phan_tram.ToArray(), tu_ngay.ToArray(), den_ngay.ToArray(), ghi_chu.ToArray()))
@@ -621,14 +659,17 @@ namespace HDQD.UCs
         private void btn_ThietLap_Click(object sender, EventArgs e)
         {
             Forms.Popup frPopup = new Forms.Popup(ucDinhNghiaCT, "QUẢN LÝ NHÂN SỰ - ĐỊNH NGHĨA CÔNG THỨC TÍNH PHỤ CẤP");
-            //if (ucDinhNghiaCT.lstDisplayString.Count > 0)
-            //{
+            if (ucDinhNghiaCT.lstDisplayString.Count > 0)
+            {
 
-            //}
+            }
             frPopup.ShowDialog();
             if (ucDinhNghiaCT.lstValueString.Count > 0)
             {
-                txt_CongThucLuong.Text = string.Join(" ", txt_CongThucLuong.Text);
+                chuoi_cong_thuc = string.Join("", ucDinhNghiaCT.lstValueString.ToArray());
+                chuoi_cong_thuc_text = string.Join(" ", ucDinhNghiaCT.lstDisplayString.ToArray());
+
+                txt_CongThucLuong.Text = chuoi_cong_thuc_text;
             }
         }
 
